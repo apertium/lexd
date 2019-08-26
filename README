c67b5c1a8e3f6a622c13dea4c025d10bc8b93f72
Daniel Swanson <popcorn.tomato.dude@gmail.com>
2019-08-26 18:41:13 -0500

    tests

--- README
+++ README
@@ -1,3 +1,15 @@
 # Lexd
 
 This is an experiment to see if I can make a faster version of a subset of HFST.
+
+To build, do
+```bash
+./autogen.sh
+make
+```
+
+To get a speed comparison, do
+```bash
+cd tests
+./timing.sh
+```
---
+++ tests/timing.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo ".twoc -> .twoc.hfst"
+time hfst-twolc test.twoc -o test.twoc.hfst
+
+echo ".lexc -> .lexc.hfst"
+time hfst-lexc test.lexc -o test.lexc.hfst
+
+echo ".lexc + .twoc"
+time hfst-invert test.lexc.hfst | hfst-compose-intersect -1 - -2 test.twoc.hfst | hfst-minimize -o test.hfst
+
+echo ".lexd"
+time ../src/lexd test.lexd test.att
